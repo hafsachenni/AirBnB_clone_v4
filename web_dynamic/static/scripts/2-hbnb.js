5be4df82fd3ab70dcef4cbed621d639ce5d27a80
hafsa chenni <hafsachenni77@gmail.com>
2024-02-29 18:19:08 +0100

    ajax

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    const checkedAmenities = {};
+    $("input[type='checkbox']").change(function () { 
+        const get_id = $(this).attr('data-id');
+        const get_name = $(this).attr('data-name');
+        if ($(this).is(':checked')) {
+            checkedAmenities = get_id;
+        }
+
+        const all_amenities = [];
+        for (const amenity in checkedAmenities) {
+            all_amenities.push(checkedAmenities[amenity]);
+        }
+        $('.amenities h4').text(all_amenities.join(', '));
+
+    });
+    $.ajax( {
+        url: 'http://0.0.0.0:5001/api/v1/status/',
+        method: 'GET',
+        success: function(response){
+            if (response.status == 'OK') {
+                $('#api_status').addClass('available');
+            }
+            else {
+                $('#api_status').removeClass('available');
+            }
+        },
+    });
+});

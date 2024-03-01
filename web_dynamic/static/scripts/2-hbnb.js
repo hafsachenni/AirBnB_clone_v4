f63ad782fb3fc51edc7534ab0311c05bb476173a
Emran <ouakouriimran@gmail.com>
2024-03-01 16:52:06 +0100

    some fixes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,35 +1,25 @@
-$(document).ready(function() {
-    const checkedAmenities = {};
-    $("input[type='checkbox']").change(function () { 
-        const get_id = $(this).attr('data-id');
-        const get_name = $(this).attr('data-name');
+$(document).ready(function () {
+    const amenity_dict = {};
+    $('input[type="checkbox"]').change(function () {
+        const data_id = $(this).attr('data-id');
+        const data_name = $(this).attr('data-name');
         if ($(this).is(':checked')) {
-            checkedAmenities[get_id] = get_name;
+            amenity_dict[data_id] = data_name;
+        } else {
+            delete amenity_dict[data_id];
         }
-        else {
-            delete checkedAmenities[get_id];
+        const amenity_list = [];
+        for (const amemity in amenity_dict) {
+            amenity_list.push(amenity_dict[amemity]);
         }
-
-        const all_amenities = [];
-        for (const amenity in checkedAmenities) {
-            all_amenities.push(checkedAmenities[amenity]);
-        }
-        $('.amenities h4').text(all_amenities.join(', '));
-
+        $('.amenities h4').text(amenity_list.join(', '));
     });
-    $.ajax( {
-        url: 'http://0.0.0.0:5001/api/v1/status/',
-        method: 'GET',
-        success: function(response){
-            if (response.status === 'OK') {
-                $('div#api_status').addClass('available');
-            }
-            else {
-                $('div#api_status').removeClass('available');
-            }
-        },
-        error: function() {
-            console.log('error in api request')
+    $.get('http://0.0.0.0/:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK')
+        {
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
         }
     });
 });
